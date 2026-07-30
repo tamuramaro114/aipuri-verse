ed21b5f4675f05a26b644939c4c54a0b2a4d9e1f
tamuramaro114 <64996954+tamuramaro114@users.noreply.github.com>
2026-07-30 09:09:50 +0900

    verse.py

--- verse.py
+++ verse.py
@@ -476,6 +476,9 @@ elif menu == "🎯 弾別コンプリート状況":
 
   # 📌 ここに新しい弾のCSVを追加していくことができます
   bullet_csv_urls = {
+      "おねがい1だん": (
+          "https://raw.githubusercontent.com/tamuramaro114/aipuri-verse/main/aipri_master/onegai1_master.csv"
+      ),
       "おねがい2だん": (
           "https://raw.githubusercontent.com/tamuramaro114/aipuri-verse/main/aipri_master/onegai2_master.csv"
       ),
