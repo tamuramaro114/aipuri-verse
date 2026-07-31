5668d9410ed6a1ea5f9d549589281bf118f2b7f4
tamuramaro114 <64996954+tamuramaro114@users.noreply.github.com>
2026-07-31 13:22:36 +0900

    verse.py

--- verse.py
+++ verse.py
@@ -486,6 +486,9 @@ elif menu == "🎯 弾別コンプリート状況":
       "おねがい3だん": (
           "https://raw.githubusercontent.com/tamuramaro114/aipuri-verse/main/aipri_master/onegai3_master.csv"  # ←必要に応じてファイル名に合わせて変更してください
       ),
+    　"リング6だん": (
+          "https://raw.githubusercontent.com/tamuramaro114/aipuri-verse/main/aipri_master/ring6_master.csv"
+      ),
   }
 
   selected_bullet_target = st.selectbox(
